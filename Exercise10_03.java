998563e3bbdac90d4015d8cc1597c5d6526eeb4c
Swugg <38431097+Swugg@users.noreply.github.com>
2021-10-15 00:24:11 -0500

    added main method

--- Exercise10_03.java
+++ Exercise10_03.java
@@ -1,9 +1,20 @@
+
 public class Exercise10_03{
     public static void main(String[]args){
-        MyInteger x = new MyInteger(15);
-        MyInteger y = new MyInteger(12);
+        String parseString = "3539";
+        char[] parseChar = {'3','5','3','9'};
+        MyInteger n1 = new MyInteger(5);
+        MyInteger n2 = new MyInteger(3539);
+        System.out.println("n1 is even? " + n1.isEven());
+        System.out.println("n1 is prime? " + n1.isPrime());
+        System.out.println("15 is prime? " + MyInteger.isPrime(15));
+        System.out.println(MyInteger.parseInt(parseChar));
+        System.out.println(MyInteger.parseInt(parseString));
+        System.out.println("n2 is odd? " + n2.isOdd());
+        System.out.println("45 is odd? " + MyInteger.isOdd(45));
+        System.out.println("n1 is equal to n2? " + n1.equals(n2));
+        System.out.println("n1 is equal to 5? " + n1.equals(5));
 
-        System.out.println(x.equals(12));
     }
 }
 
